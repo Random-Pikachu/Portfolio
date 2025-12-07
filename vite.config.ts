fba65fcb904b68f24363d8a12b130a992c8b5ded
Sangam Pratap Singh <sangampratapsingh21012006@gmail.com>
2025-12-08 02:13:47 +0530

    change

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  // base: process.env.VITE_BASE_PATH || "/Portfolio"
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    base: mode === 'production' ? '/Portfolio/' : '/',
+  }
 })
